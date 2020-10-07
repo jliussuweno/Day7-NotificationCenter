9356eeb1666e92c05a73d9c147acc800ab518a5e
Jlius Suweno <jliuss@live.com>
2020-10-07 13:47:11 +0700

    Initial Commit

---
+++ Day7-NotificationCenter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day7-NotificationCenter
+//
+//  Created by Jlius Suweno on 07/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
